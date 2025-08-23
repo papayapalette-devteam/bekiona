a11d6c200858b4af82724a7647b9a050bef008f0
digvijay315 <digvijaykumar.315@gmail.com>
2025-08-23 21:40:18 +0530

    bug foxed

--- bekiona-frotend/src/Components/api.js
+++ bekiona-frotend/src/Components/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance=axios.create({
     
         //    baseURL:'https://ln-bird-project.onrender.com/'
-           baseURL:'https://api.bekiona.com/'
+           baseURL:'https://newapi.bekiona.com/'
         // baseURL:'https://onlinerealestatecrm.in/'
         // baseURL:'http://localhost:5000/'
        
